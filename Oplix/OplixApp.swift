7c1862e7aecc9762681036da606f676ba0de0c42
Haroon <drharoon0802@gmail.com>
2025-11-17 13:13:51 -0500

    Initial Commit

---
+++ Oplix/OplixApp.swift
@@ -0,0 +1,17 @@
+//
+//  OplixApp.swift
+//  Oplix
+//
+//  Created by Hafiz Afzal on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct OplixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OplixTests/OplixTests.swift
@@ -0,0 +1,17 @@
+//
+//  OplixTests.swift
+//  OplixTests
+//
+//  Created by Hafiz Afzal on 11/17/25.
+//
+
+import Testing
+@testable import Oplix
+
+struct OplixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
